5083651db95e65674a4dd7d6ce4168ae67fd6eba
Diego Avelar <diegoarmando2011@gmail.com>
2015-08-02 01:12:43 -0300

    Mensagem de url

---
+++ index.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+<?php 
+
+require __DIR__.'/vendor/autoload.php';
+
+$url = $argv[1];
+
+if(count($argv) == 1) {
+	exit('You need to specify the Url');
+}
+
+$client = new GuzzleHttp\Client();
+$page = $client->get($url);
+
+$dom = new DOMDocument;
+
+libxml_use_internal_errors(true);
+$dom->loadHTML($page->getBody());
+libxml_clear_errors();
+
+foreach ($dom->getElementsByTagName('img') as $node)
+{
+  copy($node->getAttribute('src'), 'files/' . uniqid() . '.jpg');
+  echo 'Downloading file ' . $node->getAttribute('src') . "\n";
+}
+
